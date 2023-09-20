334f37bbb0768e8f85dc0c2963402be0a5242b65
j-sato-t <jmob0706unpc@live.jp>
2023-09-20 17:59:01 +0900

    create tickable object

---
+++ Projects/UELibs/Plugins/UELib/Source/UELib/Private/Core/TickableObject.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/TickableObject.h"
+
+UTickableObject::UTickableObject() : Super()
+{
+	bIsTickable = false;
+	bWasInit = false;
+}
+
+void UTickableObject::Tick(float DeltaTime)
+{
+	if (!bWasInit)
+	{
+		bWasInit = true;
+		Initialize();
+	}
+	OnTick(DeltaTime);
+}
+
+bool UTickableObject::IsTickable() const
+{
+	return bIsTickable;
+}
+
+bool UTickableObject::IsTickableInEditor() const
+{
+	return false;
+}
+
+bool UTickableObject::IsTickableWhenPaused() const
+{
+	return false;
+}
+
+TStatId UTickableObject::GetStatId() const
+{
+	return TStatId();
+}
+
+UWorld* UTickableObject::GetWorld() const
+{
+	return GetOuter()->GetWorld();
+}
+
+void UTickableObject::OnTick_Implementation(float DeltaTime)
+{
+}
+
+void UTickableObject::Initialize_Implementation()
+{
+}
+
+void UTickableObject::SetTickable(bool newTickable)
+{
+	bIsTickable = newTickable;
+}
---
+++ Projects/UELibs/Plugins/UELib/Source/UELib/Public/Core/TickableObject.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "TickableObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class UELIB_API UTickableObject : public UObject, public FTickableGameObject
+{
+	GENERATED_BODY()
+
+private:
+	/// <summary>
+	/// 実際に使われる動作するかの判定
+	/// </summary>
+	bool bIsTickable;
+
+	bool bWasInit;
+
+protected:
+
+	
+public:
+	UTickableObject();
+
+	// override
+	void Tick(float DeltaTime) override;
+	bool IsTickable() const override;
+	bool IsTickableInEditor() const override;
+	bool IsTickableWhenPaused() const override;
+	TStatId GetStatId() const override;
+	UWorld* GetWorld() const override;
+
+protected:
+	UFUNCTION(BlueprintNativeEvent, Category=Tick, DisplayName="Tick")
+	void OnTick(float DeltaTime);
+	virtual void OnTick_Implementation(float DeltaTime);
+
+	/// <summary>
+	/// そのオブジェクトの初回Tickで呼ばれる
+	/// </summary>
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = Tick)
+	void Initialize();
+	virtual void Initialize_Implementation();
+
+	UFUNCTION(BlueprintCallable, Category=Tick)
+	void SetTickable(bool newTickable);
+};
