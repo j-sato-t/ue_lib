ee882efbb2ab844ed5fad0b0c264025859e89ab4
j-sato-t <jmob0706unpc@live.jp>
2023-09-21 17:39:12 +0900

    create log wrapper

---
+++ Projects/UELibs/Plugins/UELib/Source/UELib/Private/Core/Logger.cpp
@@ -0,0 +1,89 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/Logger.h"
+
+DEFINE_LOG_CATEGORY(LogUELib)
+
+
+ULogger::ULogger()
+{
+	_tag = TEXT("Default");
+	_filterType = ELogLevel::Info;
+	_timeUTC = true;
+}
+
+
+
+FString ULogger::BuildLogMsg(ELogLevel level, FString msg) const
+{
+	return FString::Printf(TEXT("%s [%s] {%s}: %s"), *GetTimeString(), *GetLevelString(level), *_tag, *msg);
+}
+
+FString ULogger::GetLevelString(ELogLevel level) const
+{
+	FString full = UEnum::GetValueAsString(level);
+	FString l, r;
+	full.Split(TEXT("::"), &l, &r);
+	return r;
+}
+
+FString ULogger::GetTimeString() const
+{
+	FDateTime now = _timeUTC ? FDateTime::UtcNow() : FDateTime::Now();
+	return now.ToIso8601();
+}
+
+bool ULogger::CheckAndBuildMsg(ELogLevel level, FString msg, FString& outMsg) const
+{
+	if (level < _filterType) return false;
+	outMsg = BuildLogMsg(level, msg);
+	OnRelayLogString.Broadcast(outMsg);
+	return true;
+}
+
+void ULogger::LogTrace(FString msg) const
+{
+	FString lm;
+	if (CheckAndBuildMsg(ELogLevel::Trace, msg, lm)) UE_LOG(LogUELib, Log, TEXT("%s"), *lm);
+}
+
+void ULogger::LogDebug(FString msg) const
+{
+	FString lm;
+	if (CheckAndBuildMsg(ELogLevel::Debug, msg, lm)) UE_LOG(LogUELib, Log, TEXT("%s"), *lm);
+}
+
+void ULogger::LogInfo(FString msg) const
+{
+	FString lm;
+	if (CheckAndBuildMsg(ELogLevel::Info, msg, lm)) UE_LOG(LogUELib, Log, TEXT("%s"), *lm);
+}
+
+void ULogger::LogWarning(FString msg) const
+{
+	FString lm;
+	if (CheckAndBuildMsg(ELogLevel::Warning, msg, lm)) UE_LOG(LogUELib, Warning, TEXT("%s"), *lm);
+}
+
+void ULogger::LogError(FString msg) const
+{
+	FString lm;
+	if (CheckAndBuildMsg(ELogLevel::Error, msg, lm)) UE_LOG(LogUELib, Error, TEXT("%s"), *lm);
+}
+
+void ULogger::LogCritical(FString msg, bool bIsClash) const
+{
+	FString lm;
+	if (CheckAndBuildMsg(ELogLevel::Critical, msg, lm))
+	{
+		if (bIsClash)
+		{
+			UE_LOG(LogUELib, Fatal, TEXT("%s"), *lm);
+		}
+		else
+		{
+			UE_LOG(LogUELib, Error, TEXT("%s"), *lm);
+		}
+	}
+}
---
+++ Projects/UELibs/Plugins/UELib/Source/UELib/Public/Core/Logger.h
@@ -0,0 +1,77 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Logger.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogUELib, Log, All);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FRelayLogString, FString, logString);
+
+UENUM(BlueprintType)
+enum class ELogLevel : uint8
+{
+	Trace = 0,
+	Debug,
+	Info,
+	Warning,
+	Error,
+	Critical,
+};
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class UELIB_API ULogger : public UObject
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(BlueprintReadOnly, Meta = (ExposeOnSpawn = true))
+	ELogLevel _filterType;
+	UPROPERTY(BlueprintReadOnly, Meta = (ExposeOnSpawn = true))
+	FString _tag;
+	UPROPERTY(BlueprintReadOnly, Meta = (ExposeOnSpawn = true))
+	bool _timeUTC;
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = Log)
+	FRelayLogString OnRelayLogString;
+
+public:
+	ULogger();
+
+private:
+
+	/// <summary>
+	/// ログ文字列を生成する
+	/// </summary>
+	/// <param name="level">ログレベル</param>
+	/// <param name="msg">メッセージの内容</param>
+	/// <returns></returns>
+	FString BuildLogMsg(ELogLevel level, FString msg) const;
+
+	FString GetLevelString(ELogLevel level) const;
+
+	FString GetTimeString() const;
+
+	bool CheckAndBuildMsg(ELogLevel level, FString msg, FString& outMsg) const;
+
+public:
+
+	// =================================
+
+	UFUNCTION(BlueprintCallable, Category=Log)
+	void LogTrace(FString msg) const;
+	UFUNCTION(BlueprintCallable, Category = Log)
+	void LogDebug(FString msg) const;
+	UFUNCTION(BlueprintCallable, Category = Log)
+	void LogInfo(FString msg) const;
+	UFUNCTION(BlueprintCallable, Category = Log)
+	void LogWarning(FString msg) const;
+	UFUNCTION(BlueprintCallable, Category = Log)
+	void LogError(FString msg) const;
+	UFUNCTION(BlueprintCallable, Category = Log)
+	void LogCritical(FString msg, bool bIsClash = false) const;
+};
